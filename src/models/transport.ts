b2066942995a36e48aa3b36d60940d96f778080b
zaki <z.goumri@esi-sba.dz>
2024-12-19 23:57:52 +0100

    transport schema

---
+++ src/models/transport.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface Itransport extends Document{
+  way:string,
+  station:String ,
+  start_time:Date,
+  end_time: Date, 
+}
+
+const transportSchema = new Schema<Itransport>({
+  way:{
+    type:String,
+    require:true,
+  },
+  station:{
+    type:String,
+    require:true,
+  },
+start_time:{
+  type:Date,
+  require:true,
+}  ,
+end_time:{
+  type:Date,
+  require:true,
+}
+})
+
+export default mongoose.model<Itransport>("Transport",transportSchema );
