3c8e4c908bec6eaf90eeb500caeac4a5835a8103
zaki <z.goumri@esi-sba.dz>
2024-12-19 21:50:07 +0100

    first push

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+import mongoose from 'mongoose';
 
-const port=process.env.PORT || 3000;
+dotenv.config()
 const app=express();
-app.listen(port,()=>{
-console.log('Server is running on port '+port);
-})
 
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-});
 
+
+const port=process.env.PORT || 3000;
+const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017/express-mongo';
+
+mongoose.connect(dbURI).then(()=>{
+    console.log('Connected to database');
+}
+).catch((err)=>{
+    console.log('Error connecting to database');
+    console.log(err);
+})
+
+
+app.listen(port,()=>{
+    console.log('Server is running on port '+port);
+    })
+    
