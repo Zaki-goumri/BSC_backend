733baa4f1a4fb8e71952e894ef313d3e847fee14
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2024-12-20 18:27:45 +0100

    Added Employees gestion

---
+++ src/models/employe.ts
@@ -0,0 +1,36 @@
+import {Document, Schema, model} from 'mongoose';
+export interface Iemploye extends Document{
+  FirstName:string,
+  LastName:string,
+  DateOfBirth:Date,
+  PlaceofBirth:string,
+  Rank:String,
+  Job:string,
+}
+const employeSchema=new Schema({
+  FirstName:{
+    type:String,
+    required:true
+  },
+  LastName:{
+    type:String,
+    required:true
+  },
+  DateOfBirth:{
+    type:Date,
+    required:true
+  },
+  PlaceofBirth:{
+    type:String,
+    required:true
+  },
+  Rank:{
+    type:String,
+    required:true
+  },
+  Job:{
+    type:String,
+    required:true
+  }
+}) ;
+export const employeModel = model<Iemploye>('Employe',employeSchema);
