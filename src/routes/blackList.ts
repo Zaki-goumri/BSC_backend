674015a172f030e4f491433fff2c8f9183d1f6af
Mouloud Hasrane <m.hasrane@esi-sba.dz>
2024-12-20 07:17:49 +0100

    saved the blackList

---
+++ src/routes/blackList.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { addToBlackList, getBlackList, removeFromBlackList } from "../services/blackListService";
+
+export const BlackListRouter=Router(); 
+BlackListRouter.get('/',async(req,res)=>{
+  const output=await getBlackList();
+  res.status(output.Status).send(output.data);
+})
+
+BlackListRouter.post('/',async(req,res)=>{
+  const output=await addToBlackList(req.body.userId,req.body.Reason,req.body.Facility);
+  res.status(output.Status).send(output.data);
+})
+BlackListRouter.delete('/',async(req,res)=>{
+  const output=await removeFromBlackList(req.body.userId);
+  res.status(output.Status).send(output.data);
+})
+
